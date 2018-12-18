3898104840cb4ce15e9af0e7e8612a78f21cac32
a.vorushenko <a.vorushenko@itransition.com>
2018-12-18 13:27:48 +0300

    Added basic project structure

---
+++ src/main/java/com/itransition/partylist/PartyListApplication.java
@@ -0,0 +1,14 @@
+package com.itransition.partylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyListApplication.class, args);
+	}
+
+}
+
