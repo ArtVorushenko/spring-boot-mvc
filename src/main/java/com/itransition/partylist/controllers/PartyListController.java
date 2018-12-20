ee698afa48225ce04bb61ca116349c0ba0249dc6
a.vorushenko <a.vorushenko@itransition.com>
2018-12-20 09:32:45 +0300

    Added validation for form.

--- src/main/java/com/itransition/partylist/controllers/PartyListController.java
+++ src/main/java/com/itransition/partylist/controllers/PartyListController.java
@@ -3,10 +3,13 @@ package com.itransition.partylist.controllers;
 import com.itransition.partylist.models.JoinPartyRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class PartyListController {
@@ -18,9 +21,15 @@ public class PartyListController {
     }
 
     @PostMapping("/addToList")
-    public String confirmInvitation(@ModelAttribute JoinPartyRequest request, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("name", request.getName());
-        return "redirect:/success";
+    public String confirmInvitation(@ModelAttribute @Valid JoinPartyRequest request,
+                                    BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("error", "Some of the fields has invalid values.");
+            return "redirect:/";
+        } else {
+            redirectAttributes.addFlashAttribute("name", request.getName());
+            return "redirect:/success";
+        }
     }
 
     @GetMapping("/success")
